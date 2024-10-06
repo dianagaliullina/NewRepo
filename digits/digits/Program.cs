86d5d25dbcdeba71046bed96709d7d6f3b0aff86
Diana.Galiullina@urfu.me <Diana.Galiullina@urfu.me>
2024-10-06 22:38:34 +0500

    Задание 3 выполнено.

---
+++ digits/digits/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace digits
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите трехзначное число: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int hundreds = n / 100;
+            int tens = (n / 10) % 10;
+            int units = n % 10;
+            int x = hundreds * 100 + units * 10 + tens;
+
+
+            Console.WriteLine(x);
+
+            Console.ReadKey();
+        }
+    }
+}
