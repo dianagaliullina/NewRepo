fcb0b9973fb4c6c76729c00e79dce7d8ba2ec56f
Diana.Galiullina@urfu.me <Diana.Galiullina@urfu.me>
2024-10-20 22:35:07 +0500

    задание 6 выполнено

---
+++ task6.2/task6.2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task6._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = "клоун";
+            var word1 = s.Substring(0, 1);
+            word1 += s.Substring(3, 1);
+            word1 += s.Substring(1, 2);
+            word1 += s.Substring(4, 1);
+
+            Console.WriteLine(word1);
+
+            var word2 = s.Substring(0, 1);
+            word2 += s.Substring(2, 1);
+            word2 += s.Substring(1, 1);
+            word2 += s.Substring(3, 1);
+            word2 += s.Substring(4, 1);
+
+            Console.WriteLine(word2);
+            Console.ReadKey();
+        
+        }
+    }
+}
