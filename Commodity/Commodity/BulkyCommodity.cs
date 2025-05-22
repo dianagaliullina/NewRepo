20e567dad92517fe3cc6db47814978bf4c7d3fd5
Diana.Galiullina@urfu.me <Diana.Galiullina@urfu.me>
2025-05-22 15:25:28 +0500

    Задание 14 выполнено.

---
+++ Commodity/Commodity/BulkyCommodity.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Commodity
+{
+    public class BulkyCommodity : CommodityRealization
+    {
+        public double Length { get; set; }
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public BulkyCommodity(string article, string name, decimal wholesalePrice,
+                             decimal retailPrice, CommodityUnit unit,
+                             double length, double width, double height)
+            : base(article, name, wholesalePrice, retailPrice, unit)
+        {
+            if (length <= 0 || width <= 0 || height <= 0)
+                throw new ArgumentException("Габариты должны быть положительными");
+
+            Length = length;
+            Width = width;
+            Height = height;
+        }
+
+        public override string[] GetInfo()
+        {
+            var baseInfo = base.GetInfo();
+            var info = new string[baseInfo.Length + 1];
+
+            Array.Copy(baseInfo, info, baseInfo.Length);
+            info[info.Length - 1] = $"Габариты: {Length} x {Width} x {Height} м";
+
+            return info;
+        }
+    }
+}
---
+++ Commodity/ExtendedCommodityTests/ExtendedCommodityTests.cs
@@ -0,0 +1,84 @@
+﻿using NUnit.Framework;
+using Commodity;
+using System;
+
+namespace CommodityLibrary.UnitTests
+{
+    [TestFixture]
+    public class ExtendedCommodityTests
+    {
+        [Test]
+        public void FragileCommodityConstructorTest()
+        {
+            var item = new FragileCommodity("F001", "Хрустальные вазы", 1000m, 1500m,
+                                           CommodityUnit.Pieces, 5);
+
+            Assert.That(item.Article, Is.EqualTo("F001"));
+            Assert.That(item.MaxStackCount, Is.EqualTo(5));
+        }
+
+        [Test]
+        public void FragileCommodityGetInfoTest()
+        {
+            var item = new FragileCommodity("F001", "Хрустальные вазы", 1000m, 1500m,
+                                          CommodityUnit.Pieces, 5);
+            item.Description = "Хрупкие изделия";
+            item.StockQuantity = 10;
+
+            var info = item.GetInfo();
+
+            Assert.That(info.Length, Is.EqualTo(4));
+            Assert.That(info[3], Is.EqualTo("Максимальное количество в стопке: 5 шт."));
+        }
+
+        [Test]
+        public void PerishableCommodityConstructorTest()
+        {
+            var item = new PerishableCommodity("P001", "Молоко", 50m, 70m,
+                                             CommodityUnit.Packages, TimeSpan.FromDays(7));
+
+            Assert.That(item.Article, Is.EqualTo("P001"));
+            Assert.That(item.MaxShelfLife, Is.EqualTo(TimeSpan.FromDays(7)));
+        }
+
+        [Test]
+        public void PerishableCommodityGetInfoTest()
+        {
+            var item = new PerishableCommodity("P001", "Молоко", 50m, 70m,
+                                             CommodityUnit.Packages, TimeSpan.FromDays(7));
+            item.Description = "Пастеризованное";
+            item.StockQuantity = 100;
+
+            var info = item.GetInfo();
+
+            Assert.That(info.Length, Is.EqualTo(4));
+            Assert.That(info[3], Is.EqualTo("Максимальный срок хранения: 7 дней"));
+        }
+
+        [Test]
+        public void BulkyCommodityConstructorTest()
+        {
+            var item = new BulkyCommodity("B001", "Диван", 5000m, 7500m,
+                                         CommodityUnit.Pieces, 2.1, 0.9, 0.8);
+
+            Assert.That(item.Article, Is.EqualTo("B001"));
+            Assert.That(item.Length, Is.EqualTo(2.1));
+            Assert.That(item.Width, Is.EqualTo(0.9));
+            Assert.That(item.Height, Is.EqualTo(0.8));
+        }
+
+        [Test]
+        public void BulkyCommodityGetInfoTest()
+        {
+            var item = new BulkyCommodity("B001", "Диван", 5000m, 7500m,
+                                        CommodityUnit.Pieces, 2.1, 0.9, 0.8);
+            item.Description = "Угловой диван";
+            item.StockQuantity = 5;
+
+            var info = item.GetInfo();
+
+            Assert.That(info.Length, Is.EqualTo(4));
+            Assert.That(info[3], Is.EqualTo("Габариты: 2,1 x 0,9 x 0,8 м"));
+        }
+    }
+}
